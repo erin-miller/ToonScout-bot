ad64ecdfdc5b4008f1a66d3638b892d4485036ff
erin.miller <erinm1018@gmail.com>
2025-02-14 14:30:10 -0600

    app: reconstruct to modular files

---
+++ js/middlewares/cors.js
@@ -0,0 +1,19 @@
+import cors from 'cors';
+
+const allowedOrigins = ['https://scouttoon.info', 'https://api.scouttoon.info', 'https://staging.scouttoon.info'];
+
+const corsMiddleware = cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        } else {
+            const msg = 'The CORS policy does not allow access from this origin.';
+            return callback(new Error(msg), false);
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+});
+
+export default corsMiddleware;
